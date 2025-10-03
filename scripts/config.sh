4da83a8880d87cb9a17ca48d06015c90c3e4d821
KunzhaoRen <rkzdtc@gmail.com>
2025-10-03 17:50:39 +0000

    go2 deploy

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,7 +7,7 @@ mkdir -p $PROJECT_DIR/src
 # Unitree Model
 cd $PROJECT_DIR/src
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
-sudo apt-get install git-lfs
+apt-get install git-lfs
 git lfs install
 git clone https://huggingface.co/datasets/unitreerobotics/unitree_model
 
@@ -15,8 +15,15 @@ git clone https://huggingface.co/datasets/unitreerobotics/unitree_model
 cd $PROJECT_DIR/src
 git clone https://github.com/unitreerobotics/unitree_rl_lab.git
 /workspace/isaaclab/_isaac_sim/python.sh -m pip install -e unitree_rl_lab/source/unitree_rl_lab
+
 # Compile the robot_controller
-cd unitree_rl_lab/deploy/robots/g1_29dof # or other robots
+cd $PROJECT_DIR/src/unitree_rl_lab/deploy/robots/g1_29dof
+mkdir build
+cd build
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+make -j$(nproc)
+
+cd $PROJECT_DIR/src/unitree_rl_lab/deploy/robots/go2
 mkdir build
 cd build
 cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
