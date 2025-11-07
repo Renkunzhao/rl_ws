ba9b564ca5c1760232edece03bd9e45ed6a442db
KunzhaoRen <rkzdtc@gmail.com>
2025-11-07 20:00:49 +0000

    g1 deploy

--- scripts/setup_local.sh
+++ scripts/setup_local.sh
@@ -3,8 +3,9 @@ echo "Setup unitree ros2 simulation environment"
 source /opt/ros/humble/setup.bash
 source $WORKSPACE/install/setup.bash
 sudo ip link set dev lo multicast on
-export ROS_DOMAIN_ID=0 # 修改domain id 与仿真一致
+export ROS_DOMAIN_ID=1 # 修改domain id 与仿真一致
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
                             <NetworkInterface name="lo" priority="default" multicast="default" />
                         </Interfaces></General></Domain></CycloneDDS>'
+ros2 topic list
